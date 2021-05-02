32fe24cda7e53b7f14121d6bd2d4a5300cc22396
Ben Langley <ben.langley@deuxfractales.com>
2021-05-02 16:01:05 -0400

    navbar and sass file initialized

---
+++ frontend/src/components/Nav.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </div>
+</template>
+<style></style>
+<script>
+export default {
+  name: "Nav",
+};
+</script>
