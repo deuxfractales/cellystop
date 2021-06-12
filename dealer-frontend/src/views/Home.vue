5888a9daef4ead74e0318a1c065196fe9a719674
Ben Langley <ben.langley@deuxfractales.com>
2021-06-11 22:45:41 -0400

    started admin panel

---
+++ dealer-frontend/src/views/Home.vue
@@ -0,0 +1,39 @@
+<template>
+
+
+  <div>
+    <div class="header">CELLYSTOP-ADMIN</div>
+    <div class="content-holder">
+      <div class="w-layout-grid grid">
+        <button>
+        <h1
+          id="w-node-_9d781db9-f266-fb33-1392-8b1b6db03460-4b061c7c"
+          class="h1"
+        >
+        ADD-A-PLAN/DISCOUNT
+        </h1>
+          </button>
+        <button>
+        <h1
+          id="w-node-_04327146-d7ca-0df9-4f6a-a9c12290a1a1-4b061c7c"
+          class="h1"
+        >
+        PLANS
+        </h1></button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+export default {
+  name: "Home",
+  components: {
+  },
+};
+</script>
+<style>
+@import '../assets/cellystop-admin.webflow.css' 
+@import '../assets/webflow.css' 
+</style>
