4256b69cdef0cff0d6dc21ca88ff44a01e993cf3
Ben <ben.langley@deuxfractales.com>
2021-08-23 23:07:44 -0300

    basic functionality for admin frontend mockup is complete. TODO: troubleshoot issue where you can't go back to previous section and change your selection after you have selected an option in the following section

--- customer-frontend/src/components/Dealer-Form.vue
+++ customer-frontend/src/components/Dealer-Form.vue
@@ -1,35 +1,96 @@
-
 <script>
-import {h, ref} from "vue"
-import axios from "axios"
-// import _ from "lodash"
+import { h, ref } from "vue";
+import axios from "axios";
+import dayjs from "dayjs"
+import _ from "lodash";
+
 export default {
-    setup () {
-        const rows = ref()
-        const getFormInfo = () =>{
-            axios.get("http://localhost:3001/assets/dealer-form")
-            .then(function(response){
-                
-                //console.log(response.data)
-            rows.value = response.data 
-            console.log(rows.value)
-            rows.value.forEach(row => console.log(row.title))
-            })
-            .catch(function(error) {console.log(error)})
-            
+  setup() {
+    const rows = ref();
+    const selectedTemp = ref([]);
+    //const stepNumer = ref(1)
+    //const selected = ref([]);
+    const getFormInfo = () => {
+      axios
+        .get("http://localhost:3001/assets/dealer-form")
+        .then(function (response) {
+          //console.log(response.data)
+          rows.value = response.data;
+          //console.log(rows.value)
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+    };
+    getFormInfo();
+
+     const checkArray = (selectedObject) => {
+       let arrLength = selectedTemp.value.length;
+       let x = selectedTemp.value
+       let y = selectedObject
+      for (let i = 1; i < arrLength; i++){
+        if(x[i-1].name === y.name){
+          _.remove(x, function(n) {
+            return n.time < y.time
+          })  
         }
-        getFormInfo()
-        
-        // const createDivs = () =>{
-        //     let rawRows = rows.value
-        //     rawRows.forEach(row => console.log(row))
-        // }
-        //createDivs()
-        return () => h("div", {}, "Hello")
-    }
-}
+      }
+      //console.log(selectedTemp.value) 
+     };
+
+      const setStyles = (selectedObject) => {
+        let pID = selectedObject.idOfParent
+        let cID = selectedObject.id
+        document.getElementById(pID).style.backgroundColor = "grey"
+        document.getElementById(cID).style.color = "green"
+      }
+
+    return () =>
+      h("div", {}, [
+        h("div", [
+          rows.value?.map((row,index) => {
+            //console.log(index)
+            let y = [];
+            for (const option in row.options) {
+              y.push(row.options[option]);
+            }
+            //console.log(y);
+            return h("div",{id:`parent${index}`}, [
+              h("h1", row.title),
+
+              //h("div",{class: "option"},[row.options])
+              h("div", [
+                y.map((option,index) => {
+                  return h(
+                    "div",
+                    { id:`${index}${option}`,
+                      onClick: (event) => {
+                        let selectedObject = {
+                          name: event.path[2].children[0].innerHTML,
+                          value: event.target.innerHTML,
+                          time: dayjs().unix(),
+                          idOfParent: event.path[2].id,
+                          id: event.target.id 
+                        };
+                        selectedTemp.value.push(selectedObject);
+                        checkArray(selectedObject)
+                        setStyles(selectedObject)
+                      },
+                    },
+                    option
+                  );
+                }),
+              ]),
+            ]);
+          }),
+        ]),
+      ]);
+  },
+};
 </script>
 
-<style lang="scss" scoped>
-
+<style scoped>
+.option {
+  margin: 30px;
+}
 </style>
