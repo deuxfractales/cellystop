62dec84778f752ae562042d0a17b03054df2aa87
Ben <ben.langley@deuxfractales.com>
2021-09-10 01:13:54 -0300

    any type of plan can now be added to the database

--- services/plans/addPlans.js
+++ services/plans/addPlans.js
@@ -1,6 +1,10 @@
 async function addPlans(fastify, options) {
-    fastify.post('/addPlans', async(request, reply) => {
+    fastify.post('/addPlans/:provider', async(request, reply) => {
         //console.log(request.body[0])
+        provider = request.params.provider
+        db = fastify.mongo.client.db('Providers')
+        const collection = db.collection(provider) 
+        
         let formArray = request.body
         let y = [] 
         formArray.forEach(el => { 
@@ -9,7 +13,17 @@ async function addPlans(fastify, options) {
             y.push(x)
         })
         const plan = Object.fromEntries(y)
-        console.log(plan)
+        //console.log(plan)
+
+        try {
+            const result = await collection.insertOne(plan);
+            console.log(`A document was inserted with the _id: ${result.insertedId}`);
+        } catch (error) {
+            console.log(error)
+        } finally {
+            await fastify.close
+        }
+
         
         return ("Got Plan")
     })
